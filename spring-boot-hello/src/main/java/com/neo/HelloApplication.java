98832fba558c6617e7c9257d26fac380db1b6fc3
frankie <franklinyang2013@gmail.com>
2019-05-28 18:22:41 +0800

    ff

--- spring-boot-hello/src/main/java/com/neo/HelloApplication.java
+++ spring-boot-hello/src/main/java/com/neo/HelloApplication.java
@@ -2,11 +2,31 @@ package com.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.AbstractContextLoaderInitializer;
 
 @SpringBootApplication
 public class HelloApplication {
 
 	public static void main(String[] args) {
+
+//		fj0ef0er9gk90erg();
 		SpringApplication.run(HelloApplication.class, args);
+	}
+
+
+	public static void fj0ef0er9gk90erg(){
+
+		Object obj = new Object();
+		Integer ii = new Integer(111);
+
+		System.out.println("Object: " + obj.getClass().isAssignableFrom(Object.class));
+		System.out.println("Integer: " + ii.getClass().isAssignableFrom(Object.class));
+
+		System.out.println("Object: " + obj.getClass().isAssignableFrom(Integer.class));
+		System.out.println("Object: " + obj.getClass().isAssignableFrom(Float.class));
+
+
+		AbstractContextLoaderInitializer wefwefwef;
+
 	}
 }
